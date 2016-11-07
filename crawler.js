8087aaff3b1d997bd29ab3db11d5c5aedcbb3d39
Abel-Fan <842615663@qq.com>
2016-11-07 18:44:39 +0800

    1212
    121

---
+++ crawler.js
@@ -0,0 +1,15 @@
+require("yfserver");
+var http = require("http");
+var url = "http://www.imooc.com/video/7965";
+
+http.get(url,function(res){
+    var html = "";
+    res.on("data",function(error,data){
+        html += data;
+    })
+    res.on("end",function(){
+        console.log(html);
+    })
+}).on("error",function(){
+    console.log("获取失败");
+});
